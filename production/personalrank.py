3ccbf60f6dbb256f4f58bc35da3c9bec7a9b1654
GitHub-00 <AA19900@126.com>
2019-05-23 21:27:07 +0800

    PR learning with matrix

--- production/personalrank.py
+++ production/personalrank.py
@@ -1,10 +1,14 @@
 
 from __future__ import division
+from scipy.sparse.linalg import gmres
 import operator
 import sys
+import numpy as np
 
 sys.path.append('../util')
 import util.readdata as read
+import util.matrixutil as mat_util
+
 
 def personal_rank(graph, root, alpha, iter_num, recom_num=10 ):
     '''
@@ -51,6 +55,45 @@ def personal_rank(graph, root, alpha, iter_num, recom_num=10 ):
             break
     return  recom_result
 
+def personal_rank_matrix(graph, root, alpha, recom_num=10):
+    '''
+    Args:
+        graph: user and item graph
+        root: fix user to recom
+        alpha: the prob to random walk
+        recom_num: recom item number
+    Return:
+        a dic: key: itemid, value: pr score
+    '''
+    m, vertex, address_dict = mat_util.graph_to_matrix(graph)
+    if root not in address_dict:
+        return {}
+
+    print(vertex)
+    score_dict = {}
+    recom_dict = {}
+    mat_all = mat_util.get_matrix_all_point(m, vertex, graph)
+    index = address_dict[root]
+    initial_list = [[0] for row in range(len(vertex))]
+    initial_list[index] = 1
+    r_zero = np.array(initial_list)
+    res = gmres(mat_all, r_zero, tol=1e-8)[0]
+
+    for index in range(len(res)):
+        point = vertex[index]
+        if len(point.strip().split('_'))<2:
+            continue
+        if point in graph[root]:
+            continue
+
+        score_dict[point] = round(res[index],3)
+
+    for zuhe in sorted(score_dict.items(),key=operator.itemgetter(1), reversed=True)[:recom_num]:
+        point, score = zuhe[0], zuhe[1]
+        recom_dict[point] = score
+
+    return recom_dict
+
 
 def get_one_user_recom():
     '''
@@ -76,5 +119,22 @@ def get_one_user_recom():
         print(item_info[item])
         print(recom_result[itemid])
 
+def get_one_user_by_matrix():
+    '''
+    test one user recom
+    '''
+    user = '32'
+    alpha = 0.8
+    iter_num = 100
+    graph = read.get_graph_from_data('../data/ratings.csv')
+    recom_result = personal_rank_matrix(graph,user,alpha,iter_num)
+
 if __name__=='__main__':
-    get_one_user_recom()
+    #get_one_user_recom()
+    recom_result_ori = get_one_user_recom()
+    recom_result_mat = get_one_user_by_matrix()
+    num = 0
+    for ele in recom_result_ori:
+        if ele in recom_result_mat:
+            num += 1
+    print(num)
---
+++ util/matrixutil.py
@@ -0,0 +1,81 @@
+
+from __future__ import division
+from scipy.sparse import coo_matrix
+import numpy as np
+import sys
+import util.readdata as read
+
+
+def graph_to_matrix(graph):
+    '''
+    Args:
+        graph: user item graph
+    Retrun:
+        a coo_matrix, sparse matrix m
+        a list, total user item point
+        a dict, map all the point to row index
+    '''
+
+    vertex = list(graph.keys())
+    address_dict = {}
+    total_len = len(vertex)
+    for idx in range(total_len):
+        address_dict[vertex[idx]] = idx
+
+    row = []
+    col = []
+    data = []
+    for element_i in graph:
+        weight = round(1/len(graph[element_i]),3)
+        row_index = address_dict[element_i]
+        for element_j in graph[element_i]:
+            col_index = address_dict[element_j]
+            row.append(row_index)
+            col.append(col_index)
+            data.append(weight)
+
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+
+    matrix = coo_matrix((data,(row,col)), shape = (total_len,total_len))
+    return matrix, vertex, address_dict
+
+
+def get_matrix_all_point(matrix, vertex, alpha):
+    '''
+    Args:
+        matrix:
+        vertex: total user and item point
+        alpha: the prob for random walk
+    Return:
+        a sparse matrix
+    '''
+    total_len = len(vertex)
+    row = []
+    col = []
+    data = []
+    print(total_len)
+    for index in range(total_len):
+        row.append(index)
+        col.append(index)
+        data.append(1)
+
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+
+    eye_t = coo_matrix((data,(row,col)), shape = (total_len,total_len))
+
+    return eye_t.tocsr() - alpha * matrix.tocsr().transpose()
+
+
+
+if __name__ == '__main__':
+    graph = read.get_graph_from_data('../data/log.txt')
+    m, vertex, address_dict = graph_to_matrix(graph)
+    #print(address_dict)
+    #print(m.todense())
+    #print(vertex)
+
+    print(get_matrix_all_point(m, vertex, 0.8))
